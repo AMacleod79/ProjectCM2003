2f0ca39becce558cd178e2558f3865fdca201d00
Annabelle <couthier.annabelle@gmail.com>
2016-02-05 14:47:30 +0000

    Initial commit

---
+++ Test.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: annabelle
+ * Date: 05/02/2016
+ * Time: 14:39
+ */
+class Test
+{
+<!DOCTYPE html> <html>
+<head lang="en">
+<meta charset="UTF-8">
+<title>Deployment Test</title> </head>
+<body>
+<H1> It's ALIVE!!!</H1> </body>
+</html>
+}
